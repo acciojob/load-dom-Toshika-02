f5d4037bff30e4db1cb917e677173ef9e17f81e1
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-23 16:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+            // This function will be called when the DOM is loaded
+            const message = document.createElement('p');
+            message.textContent = 'DOM load success';
+            document.body.appendChild(message);
+        });
